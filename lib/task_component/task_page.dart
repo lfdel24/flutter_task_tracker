fbb0c9c023f3127e16bafa3ac08a2c1daddea98c
lfdel24 <lfdel24@gmail.com>
2021-05-07 11:12:46 -0500

    Maquetando

--- lib/task_component/task_page.dart
+++ lib/task_component/task_page.dart
@@ -4,7 +4,8 @@ class TaskPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Align(
+        alignment: Alignment.topCenter,
         child: _BuilderBody(),
       ),
     );
@@ -16,26 +17,68 @@ class _BuilderBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 600,
-      height: 300,
-      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
           border: Border.all(
         color: Colors.black,
       )),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          Text(
-            "Task Tracker",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Task Tracker",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              Expanded(child: Container()),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Add"),
+              )
+            ],
           ),
-          Expanded(child: Container()),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Add"),
-          )
+          SizedBox(height: 16),
+          _BuilderListView(),
         ],
+      ),
+    );
+  }
+}
+
+class _BuilderListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (_, i) => Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[299], borderRadius: BorderRadius.circular(8)),
+          margin: EdgeInsets.only(top: 4),
+          height: 100,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "aljsdasdasjdoajsd",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(child: Container()),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete),
+                  )
+                ],
+              ),
+              Text("aljsdasdasjdoajsdapḱedpjefdqwepqpoekqopweqpokwekop"),
+            ],
+          ),
+        ),
       ),
     );
   }
